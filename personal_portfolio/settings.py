47c65ab74db025ff4100704580ebb5d3601a37e9
a-kostyukovich <a.kostyukovich1990@gmail.com>
2021-07-05 15:06:03 +0300

    END of working day 02.07.2021

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-trx_)*_s(w9o=ts9rel8strs(mqhu8w=83q2aqo7i7wo8ww+(+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ]
 
 
 # Application definition
